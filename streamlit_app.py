fe4a4219052990ed9f8713f3a584b50afdfbf6fd
Michael Shin <30694417+canofseltzer@users.noreply.github.com>
2022-05-24 02:13:53 -0400

    Add pre-selection on picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # pick list for fruit to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
